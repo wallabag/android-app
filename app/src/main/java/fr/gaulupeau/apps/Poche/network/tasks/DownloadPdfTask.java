d6b6cbae848df812c3962f7bcfebc026c491891d
Dmitriy Bogdanov <di72nn@gmail.com>
2016-05-12 20:24:39 +0400

    Rewrite DownloadPdfTask
    Refactoring
    Add download notification

--- app/src/main/java/fr/gaulupeau/apps/Poche/network/tasks/DownloadPdfTask.java
+++ app/src/main/java/fr/gaulupeau/apps/Poche/network/tasks/DownloadPdfTask.java
@@ -1,6 +1,11 @@
 package fr.gaulupeau.apps.Poche.network.tasks;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -10,8 +15,6 @@ import java.io.IOException;
 import fr.gaulupeau.apps.InThePoche.R;
 import fr.gaulupeau.apps.Poche.entity.Article;
 import fr.gaulupeau.apps.Poche.entity.ArticleDao;
-import okhttp3.Call;
-import okhttp3.Callback;
 import okhttp3.Headers;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -20,17 +23,42 @@ import okio.Okio;
 
 public class DownloadPdfTask extends GenericArticleTask {
 
-    private File exportDir;
+    protected static String TAG = DownloadPdfTask.class.getSimpleName();
 
-    public DownloadPdfTask(Context context, int articleId, ArticleDao articleDao, Article article) {
+    private NotificationManager notificationManager;
+    private NotificationCompat.Builder notificationBuilder;
+
+    private int notificationID = 1337; // TODO: fix?
+
+    private String exportDir;
+
+    private File resultFile;
+
+    public DownloadPdfTask(Context context, int articleId, ArticleDao articleDao, Article article,
+                           String exportDir) {
         super(context, articleId, articleDao, article);
+        this.exportDir = exportDir;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+        if(context == null) return;
+
+        String title = String.format(context.getString(R.string.downloadPdfPathStart), exportDir);
+
+        notificationManager = (NotificationManager)context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationBuilder = new NotificationCompat.Builder(context)
+                .setContentTitle(title).setContentText("Download in progress") // TODO: fix text and icon
+                .setSmallIcon(R.drawable.ic_action_refresh);
     }
 
     @Override
     protected Boolean doInBackgroundSimple(Void... params) throws IOException {
-        if(isOffline || noCredentials || context == null) return false;
-
-        exportDir = context.getApplicationContext().getExternalFilesDir(null);
+        if(isOffline || noCredentials) return false;
 
         publishProgress(1); // report that we didn't stop because of isOffline or noCredentials
 
@@ -38,7 +66,10 @@ public class DownloadPdfTask extends GenericArticleTask {
         Log.d(TAG, "doInBackgroundSimple() testConn=" + testConn);
         if (testConn != 0) {
             Log.w(TAG, "doInBackgroundSimple() testing connection failed with value " + testConn);
-            // TODO: set errorMessage
+            if(context != null) {
+                // TODO: set errorMessage
+//                errorMessage = context.getString(R.string.);
+            }
             return false;
         }
 
@@ -46,64 +77,61 @@ public class DownloadPdfTask extends GenericArticleTask {
         String exportType = "pdf";
         String exportUrl = service.getExportUrl(articleId, exportType);
         Log.d(TAG, "doInBackgroundSimple() exportUrl=" + exportUrl);
-        final String exportFileName = articleTitle + "." + exportType;
+        String exportFileName = articleTitle + "." + exportType;
 
         Request request = new Request.Builder()
                 .url(exportUrl)
                 .build();
-        service.getClient().newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.i(TAG, "callback.onFailure()", e);
+
+        Response response = service.getClient().newCall(request).execute();
+
+        if(!response.isSuccessful()) {
+            throw new IOException("Unexpected code: " + response);
+        }
+
+        // do we need it?
+        if(Log.isLoggable(TAG, Log.DEBUG)) {
+            Headers responseHeaders = response.headers();
+            for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                Log.d(TAG, responseHeaders.name(i) + ": " + responseHeaders.value(i));
             }
+        }
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                Log.d(TAG, "callback.onResponse()");
+        File file = new File(exportDir, exportFileName);
+        Log.d(TAG, "Saving file " + file.getAbsolutePath());
 
-                if(!response.isSuccessful()) {
-                    throw new IOException("Unexpected code: " + response);
-                }
-
-                if(Log.isLoggable(TAG, Log.DEBUG)) {
-                    Headers responseHeaders = response.headers();
-                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-                        Log.d(TAG, responseHeaders.name(i) + ": " + responseHeaders.value(i));
-                    }
-                }
-
-                File file = new File(exportDir, exportFileName);
-                Log.d(TAG, "getExportUrl() saving file " + file.getAbsolutePath());
-
-                BufferedSink sink = null;
+        BufferedSink sink = null;
+        try {
+            sink = Okio.buffer(Okio.sink(file));
+            sink.writeAll(response.body().source());
+        } catch(Exception e) {
+            Log.w(TAG, e);
+        } finally {
+            if(sink != null) {
                 try {
-                    sink = Okio.buffer(Okio.sink(file));
-                    sink.writeAll(response.body().source());
-                } catch(Exception e) {
-                    Log.w(TAG, e);
-                } finally {
-                    if(sink != null) {
-                        try {
-                            sink.close();
-                        } catch(IOException ignored) {}
-                    }
-                    response.body().close();
-                }
-
-                Log.d(TAG, "callback.onResponse() finished");
+                    sink.close();
+                } catch(IOException ignored) {}
             }
-        });
+            response.body().close();
+        }
 
-        return true; // can't really be sure
+        resultFile = file;
+
+        Log.d(TAG, "doInBackgroundSimple finished");
+
+        return true;
     }
 
     @Override
     protected void onProgressUpdate(Integer... progress) {
+        super.onProgressUpdate();
+
+        if(context == null) return;
+
         int current = progress[0];
-        if(current >= 1) {
-            String toastTextStart = String.format(
-                    context.getString(R.string.downloadPdfPathStart), exportDir);
-            Toast.makeText(context, toastTextStart, Toast.LENGTH_LONG).show();
+        if(current == 1) {
+            notificationManager.notify(notificationID,
+                    notificationBuilder.setProgress(1, 0, true).build());
         }
     }
 
@@ -111,11 +139,26 @@ public class DownloadPdfTask extends GenericArticleTask {
     protected void onPostExecute(Boolean success) {
         super.onPostExecute(success);
 
-        if(success) return;
+        if(context == null) return;
 
-        if(isOffline && context != null) {
-            Toast.makeText(context, R.string.downloadPdf_noInternetConnection, Toast.LENGTH_SHORT)
-                    .show();
+        if(success) {
+            Intent intent = new Intent();
+            intent.setAction(android.content.Intent.ACTION_VIEW);
+            intent.setDataAndType(Uri.fromFile(resultFile), "application/pdf");
+
+            PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            notificationBuilder.setContentTitle("Article downloaded").setContentText("Touch to open") // TODO: strings
+                    .setContentIntent(contentIntent).setProgress(0, 0, false);
+
+            notificationManager.notify(notificationID, notificationBuilder.build());
+        } else {
+            notificationManager.cancel(notificationID);
+
+            if(isOffline) {
+                Toast.makeText(
+                        context, R.string.downloadPdf_noInternetConnection, Toast.LENGTH_SHORT)
+                        .show();
+            }
         }
     }
 
