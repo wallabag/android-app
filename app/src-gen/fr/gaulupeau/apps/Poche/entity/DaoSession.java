970fe309a389135fb68ec7d95153f6b8ab8db651
Sven Fischer <git-dev@linux4tw.de>
2016-10-15 16:36:24 +0200

    run greenDAO3-generator to create classes with annotations

--- app/src-gen/fr/gaulupeau/apps/Poche/entity/DaoSession.java
+++ app/src-gen/fr/gaulupeau/apps/Poche/entity/DaoSession.java
@@ -1,13 +1,12 @@
 package fr.gaulupeau.apps.Poche.entity;
 
-import android.database.sqlite.SQLiteDatabase;
-
 import java.util.Map;
 
-import de.greenrobot.dao.AbstractDao;
-import de.greenrobot.dao.AbstractDaoSession;
-import de.greenrobot.dao.identityscope.IdentityScopeType;
-import de.greenrobot.dao.internal.DaoConfig;
+import org.greenrobot.greendao.AbstractDao;
+import org.greenrobot.greendao.AbstractDaoSession;
+import org.greenrobot.greendao.database.Database;
+import org.greenrobot.greendao.identityscope.IdentityScopeType;
+import org.greenrobot.greendao.internal.DaoConfig;
 
 import fr.gaulupeau.apps.Poche.entity.Article;
 import fr.gaulupeau.apps.Poche.entity.QueueItem;
@@ -20,7 +19,7 @@ import fr.gaulupeau.apps.Poche.entity.QueueItemDao;
 /**
  * {@inheritDoc}
  * 
- * @see de.greenrobot.dao.AbstractDaoSession
+ * @see org.greenrobot.greendao.AbstractDaoSession
  */
 public class DaoSession extends AbstractDaoSession {
 
@@ -30,7 +29,7 @@ public class DaoSession extends AbstractDaoSession {
     private final ArticleDao articleDao;
     private final QueueItemDao queueItemDao;
 
-    public DaoSession(SQLiteDatabase db, IdentityScopeType type, Map<Class<? extends AbstractDao<?, ?>>, DaoConfig>
+    public DaoSession(Database db, IdentityScopeType type, Map<Class<? extends AbstractDao<?, ?>>, DaoConfig>
             daoConfigMap) {
         super(db);
 
@@ -48,8 +47,8 @@ public class DaoSession extends AbstractDaoSession {
     }
     
     public void clear() {
-        articleDaoConfig.getIdentityScope().clear();
-        queueItemDaoConfig.getIdentityScope().clear();
+        articleDaoConfig.clearIdentityScope();
+        queueItemDaoConfig.clearIdentityScope();
     }
 
     public ArticleDao getArticleDao() {
