68c155df8abda62b0d3f0329abc90eff4e421bd2
Dmitriy Bogdanov <d@di72nn.com>
2017-02-04 13:10:49 +0400

    Restore Settings screen state after activity recreation

--- app/src/main/java/fr/gaulupeau/apps/Poche/ui/preferences/SettingsActivity.java
+++ app/src/main/java/fr/gaulupeau/apps/Poche/ui/preferences/SettingsActivity.java
@@ -32,9 +32,11 @@ public class SettingsActivity extends BaseActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
-                .commit();
+        if(savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new SettingsFragment())
+                    .commit();
+        }
     }
 
     public static class SettingsFragment extends PreferenceFragment
