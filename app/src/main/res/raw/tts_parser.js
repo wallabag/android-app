30bec21ab4d42f4ebd39ce1c6d680760bbc62780
Dmitriy Bogdanov <d@di72nn.com>
2020-04-02 17:26:30 +0400

    TTS JS parser: avoid splitting text inside blocks (like paragraphs)

--- app/src/main/res/raw/tts_parser.js
+++ app/src/main/res/raw/tts_parser.js
@@ -41,31 +41,74 @@ function shouldSkip(element) {
 }
 
 function parseDocumentText() {
-    var elem = document.getElementsByTagName('body')[0];
+    var range = document.createRange();
+
+//    var stack = [];
+
+    var currentText = '';
+    var currentStartElement = null;
+    var currentEndElement = null;
+
+    var processCurrentText = function() {
+        if (currentText && currentText.trim().length > 0) {
+            range.setStartBefore(currentStartElement);
+            range.setEndAfter(currentEndElement);
+            var text = currentText.trim();
+            var rect = range.getBoundingClientRect();
+            cmdText(text, rect.top, rect.bottom);
+        }
+
+        currentText = '';
+        currentStartElement = null;
+        currentEndElement = null;
+    };
 
     var parserCallback = {
         enterNode(element) {
 //            console.log('enterNode ' + element);
+//            stack.push(element);
+//            console.log('enterNode, stack: ' + stackToString(stack));
+
+            if (isBlock(element)) {
+                processCurrentText();
+            }
         },
 
         processLeaf(element) {
 //            console.log('processLeaf ' + element);
-            if (element.nodeType == Node.TEXT_NODE && element.textContent.trim().length > 0) {
-//                console.log('TEXT: ' + element.textContent.trim());
-                range.selectNode(element);
-                var rect = range.getBoundingClientRect();
-                var text = element.textContent.trim();
-                cmdText(text, rect.top, rect.bottom);
+
+            if (element.nodeType == Node.TEXT_NODE) {
+                if (currentStartElement === null) currentStartElement = element;
+                currentText += element.textContent;
+                currentEndElement = element;
+            } else if (element.nodeName === 'BR' || isBlock(element)) {
+                processCurrentText();
             }
         },
 
         leaveNode(element) {
 //            console.log('leaveNode ' + element);
+//            if (element !== stack.pop()) console.log('POP DID NOT MATCH');
+//            console.log('leaveNode, stack: ' + stackToString(stack));
+
+            if (isBlock(element)) {
+                processCurrentText();
+            }
         }
     };
 
-    var range = document.createRange();
     cmdStart();
+
+    var elem = document.getElementsByTagName('body')[0];
     traverse(elem, parserCallback);
+
     cmdEnd();
 }
+
+function isBlock(element) {
+    return element.nodeName === 'P' || window.getComputedStyle(element).display === 'block';
+}
+
+//function stackToString(stack) {
+//    return stack.map(e => e.nodeName.toLowerCase()).join(', ');
+//}
