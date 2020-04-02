07b9acc5fa7d40d1e2ce408ba2d0c41c054c69b8
Dmitriy Bogdanov <d@di72nn.com>
2020-04-02 14:44:24 +0400

    TTS js parser: rewrite traversal

--- app/src/main/res/raw/tts_parser.js
+++ app/src/main/res/raw/tts_parser.js
@@ -1,28 +1,3 @@
-function nextDomElem(elem) {
-    var result;
-    if (elem.hasChildNodes() && elem.tagName != 'SCRIPT' && elem.tagName != 'NOSCRIPT') {
-        result = elem.firstChild;
-    } else {
-        result = elem.nextSibling;
-        while((result == null) && (elem != null)) {
-            elem = elem.parentNode;
-            if (elem != null) {
-                result = elem.nextSibling;
-            }
-        }
-    }
-    return result;
-}
-
-function nextTextElem(elem) {
-    while(elem = nextDomElem(elem)) {
-        if ((elem.nodeType == 3) && (elem.textContent.trim().length > 0)) {
-            break;
-        }
-    }
-    return elem;
-}
-
 function cmdStart() {
     hostWebViewTextController.onStart();
 }
@@ -33,15 +8,64 @@ function cmdText(text, top, bottom) {
     hostWebViewTextController.onText(text, top, bottom);
 }
 
+function traverse(element, callback) {
+    var rootElement = element;
+
+    while (element !== null) {
+        if (!shouldSkip(element) && element.hasChildNodes()) {
+            callback.enterNode(element);
+            element = element.firstChild;
+        } else {
+            var next = element.nextSibling;
+            while (next === null && element !== null) {
+                element = element.parentNode;
+                if (element === rootElement) {
+                    element = null;
+                }
+                if (element !== null) {
+                    callback.leaveNode(element);
+                    next = element.nextSibling;
+                }
+            }
+            element = next;
+        }
+
+        if (element !== null && !shouldSkip(element) && !element.hasChildNodes()) {
+            callback.processLeaf(element);
+        }
+    }
+}
+
+function shouldSkip(element) {
+    return element.tagName === 'SCRIPT' || element.tagName === 'NOSCRIPT';
+}
+
 function parseDocumentText() {
     var elem = document.getElementsByTagName('body')[0];
+
+    var parserCallback = {
+        enterNode(element) {
+//            console.log('enterNode ' + element);
+        },
+
+        processLeaf(element) {
+//            console.log('processLeaf ' + element);
+            if (element.nodeType == Node.TEXT_NODE && element.textContent.trim().length > 0) {
+//                console.log('TEXT: ' + element.textContent.trim());
+                range.selectNode(element);
+                var rect = range.getBoundingClientRect();
+                var text = element.textContent.trim();
+                cmdText(text, rect.top, rect.bottom);
+            }
+        },
+
+        leaveNode(element) {
+//            console.log('leaveNode ' + element);
+        }
+    };
+
     var range = document.createRange();
     cmdStart();
-    while(elem = nextTextElem(elem)) {
-        range.selectNode(elem);
-        var rect = range.getBoundingClientRect();
-        var text = elem.textContent.trim();
-        cmdText(text, rect.top, rect.bottom);
-    }
+    traverse(elem, parserCallback);
     cmdEnd();
 }
