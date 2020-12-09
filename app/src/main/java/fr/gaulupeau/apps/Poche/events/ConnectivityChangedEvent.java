3ac93999b08c2b2901bf6de027219350bd1ae1ab
Dmitriy Bogdanov <d@di72nn.com>
2020-12-09 15:39:24 +0400

    Simplify some code

--- app/src/main/java/fr/gaulupeau/apps/Poche/events/ConnectivityChangedEvent.java
+++ app/src/main/java/fr/gaulupeau/apps/Poche/events/ConnectivityChangedEvent.java
@@ -1,17 +1,3 @@
 package fr.gaulupeau.apps.Poche.events;
 
-public class ConnectivityChangedEvent {
-
-    private boolean noConnectivity;
-
-    public ConnectivityChangedEvent() {}
-
-    public ConnectivityChangedEvent(boolean noConnectivity) {
-        this.noConnectivity = noConnectivity;
-    }
-
-    public boolean isNoConnectivity() {
-        return noConnectivity;
-    }
-
-}
+public class ConnectivityChangedEvent {}
