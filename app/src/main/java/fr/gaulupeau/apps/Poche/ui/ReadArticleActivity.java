a50fd73931a1f8a7a6483e9a5828f3ff112dc5e8
Dmitriy Bogdanov <d@di72nn.com>
2020-12-09 16:19:11 +0400

    Revert "Replace deprecated method usage"
    This reverts commit f214a3b165a6fbf690e2a961962dfba39a9fa701.

--- app/src/main/java/fr/gaulupeau/apps/Poche/ui/ReadArticleActivity.java
+++ app/src/main/java/fr/gaulupeau/apps/Poche/ui/ReadArticleActivity.java
@@ -19,7 +19,6 @@ import android.view.WindowManager;
 import android.webkit.ConsoleMessage;
 import android.webkit.HttpAuthHandler;
 import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -677,10 +676,9 @@ public class ReadArticleActivity extends BaseActionBarActivity {
                 super.onPageFinished(view, url);
             }
 
+            @SuppressWarnings("deprecation") // can't use newer method until API 21
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                String url = request.getUrl().toString();
-
+            public boolean shouldOverrideUrlLoading(WebView webView, String url) {
                 // If we try to open current URL, do not propose to save it, directly open browser
                 if (url.equals(articleUrl)) {
                     openURL(url);
