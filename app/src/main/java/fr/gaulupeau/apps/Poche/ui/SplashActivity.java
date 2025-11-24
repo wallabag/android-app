47185b6aec185e4c9b06fa20f43d4c5c012ec232
Sven Fischer <git-dev@linux4tw.de>
2025-11-24 21:25:01 +0100

    update to sdk 36 and edge-to-edge-display

--- app/src/main/java/fr/gaulupeau/apps/Poche/ui/SplashActivity.java
+++ app/src/main/java/fr/gaulupeau/apps/Poche/ui/SplashActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
         startActivity(new Intent(this, MainActivity.class));
         finish();
